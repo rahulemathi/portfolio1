0bc24a28fb78b15cd40e3fd84b3ccb963e9945c7
Rahul <erahul096@gmail.com>
2023-02-01 21:49:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+var names = prompt("Enter Your Name");
+function name(){
+if(names.length<1){
+    location.reload();
+}else{
+    const nameId = document.getElementById("name");
+    nameId.innerHTML=`Hi ${names}`;
+}
+}
+
