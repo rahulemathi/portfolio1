bee77661ca8596149eded3031a932b96d3fc9077
Rahul <erahul096@gmail.com>
2023-02-03 14:49:50 +0530

    added intro section and projects

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 
-var names = prompt("Enter Your Name");
-function name(){
-if(names.length<1){
-    location.reload();
-}else{
-    const nameId = document.getElementById("name");
-    nameId.innerHTML=`Hi ${names}`;
-}
-}
+// var names = prompt("Enter Your Name");
+// function name(){
+// if(names.length<1){
+//     location.reload();
+// }else{
+//     const nameId = document.getElementById("name");
+//     nameId.innerHTML=`Hi ${names}`;
+//     const name1 = document.getElementById("name1");
+//     name1.innerHTML=`${names}`;
+// }
+// }
 
